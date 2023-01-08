d24b66cccc44468c51b0f5dde2888706d072b4bd
xiaoxiao <593302270@qq.com>
2023-01-08 19:57:54 +0800

    🍷 Add time pro

--- chapter1.py
+++ chapter1.py
@@ -2,7 +2,8 @@ import streamlit as slt
 import datetime
 import json
 import time
-import copy
+import pytz
+
 
 slt.set_page_config(
     page_title="奇奇怪怪的发电中心站",   
@@ -18,52 +19,159 @@ else:
 if slt.session_state.first_visit:
     slt.balloons()  #第一次访问时才会放气球
 
-
 @slt.experimental_memo
 def load_data(address):
-    temp = "hshas"
+    temp = ""
     with open(address, "r", encoding = "utf8")  as f:
         temp = json.load(f)
-        print("RUN")
     return temp
+
     
 ADDRESS = "./chathistory.json"
 CHAT_HISTORY = load_data(ADDRESS)
-slt.write(len(CHAT_HISTORY))
+START_TIMESTAMP = CHAT_HISTORY[0]['CreateTime']
+END_TIMESTAMP = CHAT_HISTORY[-1]['CreateTime']
+TOTAL_MSG = len(CHAT_HISTORY)
+
+slt.markdown("# 奇奇怪怪的聊天站")
+slt.json(CHAT_HISTORY[-1])
+
+# ============================ ==================================
 
 
 
-    
+def get_local_timestamp(date_time):
+    """ 返回本地时间的时间戳格式
 
+    Args:
+        date_time (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    time_zone = pytz.timezone('Asia/Shanghai')
+    timeArray = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+    local_dt = timeArray.astimezone(time_zone)
+    return int(time.mktime(local_dt.timetuple()))
+
+def get_msg_vol(timestamp1, timestamp2):
+    start = 0
+    end = TOTAL_MSG - 1
+    indx1 = -1
+    indx2 = -1
+    while start <= end:
+        mid = start + ((end - start) >> 1)
+        if CHAT_HISTORY[mid]['CreateTime'] > timestamp1:
+            indx1 = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    start = 0
+    end = TOTAL_MSG - 1
+    while start <= end:
+        mid = start + ((end - start) >> 1)
+        if CHAT_HISTORY[mid]['CreateTime'] > timestamp2:
+            indx2 = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    return int(abs(indx1 - indx2))
+
+
+def get_interval_time(timestamp1, timestamp2):
+    result = (timestamp2 - timestamp1) // 60 // 60 // 24
+    return result
+
+def get_local_time(timeStamp):
+    """ 从时间戳获取当地时间
+
+    Args:
+        timeStamp (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    timeArray = time.localtime(timeStamp)
+    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    return otherStyleTime
+
+
+def show_profile():
+    slt.write("总共有", str(TOTAL_MSG) ,"条消息, 最早的消息来自瑜瑜子，发送时间是", \
+        get_local_time(START_TIMESTAMP), "而最晚的消息是笑笑在", get_local_time(END_TIMESTAMP) , \
+            "发送的。", "在这", str(get_interval_time(START_TIMESTAMP, END_TIMESTAMP)), "天中，我们畅所欲言，无话不谈。"  )
+
+show_profile()
+
+d = slt.date_input(
+    "Choose Start Day",
+    datetime.date(2022, 2, 3))
+slt.write('你选择的日期 📅 是:', d)
+slt.write("Msg volume for the selected day " , d, " is ", get_msg_vol(get_local_timestamp(str(d) + " 00:00:00"), \
+     get_local_timestamp(str(d) + " 23:59:59")))
+# slt.write("Time stamp is ", str(get_local_timestamp( "2022-10-04 10:57:22")))
+# print(d)
+# ============================== ============================== 
+
+
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+
+# st.title('Uber pickups in NYC')
+
+# DATE_COLUMN = 'date/time'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# @slt.cache
+# def loads_data(nrows):
+#     data = pd.read_csv(DATA_URL, nrows=nrows)
+#     lowercase = lambda x: str(x).lower()
+#     data.rename(lowercase, axis='columns', inplace=True)
+#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     return data
+
+# data_load_state = slt.text('Loading data...')
+# data = loads_data(10000)
+# data_load_state.text("Done! (using st.cache)")
+
+# if slt.checkbox('Show raw data'):
+#     slt.subheader('Raw data')
+#     slt.write(data)
+
+# slt.subheader('Number of pickups by hour')
+# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+# slt.bar_chart(hist_values)
+
+# # Some number in the range 0-23
+# hour_to_filter = slt.slider('hour', 0, 23, 17)
+# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+# slt.subheader('Map of all pickups at %s:00' % hour_to_filter)
+# slt.map(filtered_data)
+
+
+
+# ============================== ============================== 
+# d = slt.date_input(
+#     "When\'s your birthday",
+#     datetime.date(2019, 7, 6))
+# slt.write('Your birthday is:', d)
 
 # slt.info(len(CHAT_HISTORY))
-
-
-
-
 # def run():
 # chat_history = load_data(ADDRESS)
 # slt.write(len(chat_history))
-
 # slt.title("Tutorials Streamlit")
-
 # slt.info("Hello fellow!")
-
 # slt.header("Header")
-
 # slt.subheader("SubHeader")
-
 # slt.success("Success")
-
 # slt.warning("Warning")
-
 # slt.markdown("$$  f(x) = x$$")
-
 # slt.write("Write here.")
-
-
 # # img = Image.open("picx1.png")
-
 # # slt.image(img, width= 300, caption="picx1")
 
 # chat_history = load_data(ADDRESS)
@@ -73,9 +181,7 @@ slt.write(len(CHAT_HISTORY))
 # slt.write(occupation)
 
 # location = slt.multiselect("Where do you work?", ["Beijing", "Shanghai", "Chengdu"])
-
 # slt.write("You select " , len(location), " Locations")
-
 # level = slt.slider("Whats your level?", 2,100)
 
 # first_name = slt.text_input("Your first name?", "Type here..") 
